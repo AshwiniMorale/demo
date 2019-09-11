d727cb149042b6db2caeb4a70aa46c90e668cfb8
ashwini <moralegauri7944@gmil.com>
2019-09-11 13:43:05 +0530

    first commit by ashwini

---
+++ Demo/src/com/demoproject/Abc.java
@@ -0,0 +1,14 @@
+package com.demoproject;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is add()");
+		System.out.println("this is sub()");
+		System.out.println("this is mul()");
+		System.out.println("this is div()");
+
+	}
+
+}
