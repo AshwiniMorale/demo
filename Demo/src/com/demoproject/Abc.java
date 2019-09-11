8c63ed52d7ae3d9d3b7c8af399c33198ba011da4
Admin <Admin@10.10.11.118>
2019-09-11 13:56:46 +0530

    Ye wala maine kiya hu commit Change karke

--- Demo/src/com/demoproject/Abc.java
+++ Demo/src/com/demoproject/Abc.java
@@ -4,10 +4,10 @@ public class Abc {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is add()");
-		System.out.println("this is sub()");
-		System.out.println("this is mul()");
-		System.out.println("this is div()");
+		System.out.println("this is add() one wala");
+		System.out.println("this is sub() two wala");
+		System.out.println("this is mul() three wala");
+		System.out.println("this is div() four wala");
 
 	}
 
