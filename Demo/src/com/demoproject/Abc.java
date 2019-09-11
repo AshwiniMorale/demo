e2cd4b62bc87afa3998099f659ace14609b4cc6e
ashwini <moralegauri7944@gmil.com>
2019-09-11 14:00:54 +0530

    second commit by ashwini

--- Demo/src/com/demoproject/Abc.java
+++ Demo/src/com/demoproject/Abc.java
@@ -4,10 +4,10 @@ public class Abc {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is add()");
-		System.out.println("this is sub()");
-		System.out.println("this is mul()");
-		System.out.println("this is div()");
+		System.out.println("this is add()2");
+		System.out.println("this is sub()2");
+		System.out.println("this is mul()2");
+		System.out.println("this is div()2");
 
 	}
 
