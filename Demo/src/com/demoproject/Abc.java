c9e93ab294b616fc5f6d5dc4ed3ad2f2a1755c57
ashwini <moralegauri7944@gmil.com>
2019-09-11 14:13:26 +0530

    conflict resolve

--- Demo/src/com/demoproject/Abc.java
+++ Demo/src/com/demoproject/Abc.java
@@ -8,6 +8,10 @@ public class Abc {
 		System.out.println("this is sub()2");
 		System.out.println("this is mul()2");
 		System.out.println("this is div()2");
+		System.out.println("this is add() one wala");
+		System.out.println("this is sub() two wala");
+		System.out.println("this is mul() three wala");
+		System.out.println("this is div() four wala");
 
 	}
 
